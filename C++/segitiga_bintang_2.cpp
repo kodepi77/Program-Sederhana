14426f1f25cbb99a0d18bf55eba1e136e3f8b713
Kodepi <83491514+kodepi77@users.noreply.github.com>
2021-05-03 19:48:47 +0700

    Create segitiga_bintang_2.cpp

---
+++ C++/segitiga_bintang_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int size = 5;
+
+	for(int row=1; row<=size; row++) {
+		for(int col=1; col<=size-row+1; col++) {
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+}
